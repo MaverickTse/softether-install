31b98130d395c2d9f1776357d2a8a290023f8c1c
MaverickTse <mavericktse@gmail.com>
2018-03-17 19:28:28 +0800

    initial commit

---
+++ se-build.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+cd $HOME
+if [[ ! -d "SoftEtherVPN" ]]; then
+    git clone https://github.com/SoftEtherVPN/SoftEtherVPN.git || exit $?
+fi
+cd SoftEtherVPN
+git pull || exit $?
+
+yum -y install readline-devel ncurses-devel openssl-devel
+./configure
+export DEBUG="NO"
+make || exit $?
+make install || exit $?
